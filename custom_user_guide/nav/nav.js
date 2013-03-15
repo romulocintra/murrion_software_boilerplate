66fabe79d97334bd44914616d4879b65ed76ec5d
Alejandro Bernabe <alejandro@murrion.com>
2013-03-15 12:39:12 +0100

    Structure of the custom documentation.

---
+++ custom_user_guide/nav/nav.js
@@ -0,0 +1,43 @@
+function create_menu(basepath)
+{
+	var base = (basepath == 'null') ? '' : basepath;
+
+	document.write(
+		'<table cellpadding="0" cellspaceing="0" border="0" style="width:98%"><tr>' +
+		'<td class="td" valign="top">' +
+
+		'<ul>' +
+		'<li><a href="'+base+'index.html">User Guide Home</a></li>' +
+		'<li><a href="'+base+'toc.html">Table of Contents Page</a></li>' +
+		'</ul>' +
+		
+		'</td><td class="td_sep" valign="top">' +
+
+		'</td><td class="td_sep" valign="top">' +
+
+		'<h3>Class Reference</h3>' +
+		'<ul>' +
+		'<li><a href="'+base+'libraries/email.html">Email Class</a></li>' +
+		'<li><a href="'+base+'libraries/form_validation.html">Form Validation Class</a></li>' +
+		'<li><a href="'+base+'libraries/input.html">Input Class</a></li>' +
+		'</ul>' +
+
+		'</td><td class="td_sep" valign="top">' +
+
+		'<h3>Helper Reference</h3>' +
+		'<ul>' +
+		
+		'<li><a href="'+base+'helpers/array_helper.html">Array Helper</a></li>' +
+		'<li><a href="'+base+'helpers/database_helper.html">Database Helper</a></li>' +
+		'<li><a href="'+base+'helpers/date_helper.html">Date Helper</a></li>' +
+		'<li><a href="'+base+'helpers/export_helper.html">Export Helper</a></li>' +
+		'<li><a href="'+base+'helpers/form_helper.html">Form Helper</a></li>' +
+		'<li><a href="'+base+'helpers/map_helper.html">Map Helper</a></li>' +
+		'<li><a href="'+base+'helpers/number_helper.html">Number Helper</a></li>' +
+		'<li><a href="'+base+'helpers/path_helper.html">Path Helper</a></li>' +
+		'<li><a href="'+base+'helpers/string_helper.html">String Helper</a></li>' +
+
+		'</ul>' +
+
+		'</td></tr></table>');
+}
--- sql/boilerplate.sql
+++ sql/boilerplate.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 15-03-2013 a las 10:51:10
+-- Tiempo de generación: 15-03-2013 a las 12:21:40
 -- Versión del servidor: 5.5.16
 -- Versión de PHP: 5.3.8
 
@@ -23,6 +23,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `mur_captcha`
+--
+
+CREATE TABLE `mur_captcha` (
+  `captcha_id` bigint(13) unsigned NOT NULL AUTO_INCREMENT,
+  `captcha_time` int(10) unsigned NOT NULL,
+  `ip_address` varchar(16) NOT NULL DEFAULT '0',
+  `word` varchar(20) NOT NULL,
+  PRIMARY KEY (`captcha_id`),
+  KEY `word` (`word`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `mur_email_sent`
 --
 
@@ -37,6 +52,51 @@ CREATE TABLE `mur_email_sent` (
   `email_sent_debugger` mediumtext NOT NULL,
   `email_sent_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`email_sent_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mur_user`
+--
+
+CREATE TABLE `mur_user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_type` varchar(20) NOT NULL DEFAULT '',
+  `user_email` varchar(100) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_last_name` varchar(100) NOT NULL DEFAULT '',
+  `user_password` varchar(32) NOT NULL,
+  `user_facebook_id` varchar(20) DEFAULT NULL,
+  `user_activation_token` varchar(32) NOT NULL,
+  `user_recovery_token` varchar(32) NOT NULL,
+  `user_login_attempts` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `user_email_updates` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `user_approved` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT 'used only for firm solicitors',
+  `user_active` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `user_retired` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `user_deleted` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `user_last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_created_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_modified_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mur_user_login`
+--
+
+CREATE TABLE `mur_user_login` (
+  `user_login_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `user_login_useragent` varchar(255) DEFAULT '',
+  `user_login_ip` varchar(15) DEFAULT '',
+  `user_login_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_login_id`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
