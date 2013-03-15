b49fda5f1108f1da1a77a080899d7b8d07a550dd
Paco Bernabe <paco@murrion.com>
2013-03-15 11:27:04 +0100

    New database helper

---
+++ application/helpers/database_helper.php
@@ -0,0 +1,24 @@
+function update_or_insert($table, $primary, $where, $insert)
+{
+	$CI = &get_instance();
+
+	foreach ($where as $field => $value)
+	{
+		$CI->db->where($field, $value);
+	}
+	$exists = $CI->db->get($table)->row_array();
+	
+	if($exists)
+	{
+		$CI->db->where($primary, element($primary, $exists))
+				 ->update($table, $insert);
+
+		return element($primary, $exists);
+	}
+	else
+	{
+		$CI->db->insert($table, $insert);
+
+		return $CI->db->insert_id();
+	}
+}
